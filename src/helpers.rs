0913949aa6010f5b01b9b0c555b962936b5e13e5
SpiralP <spiral.palmer@gmail.com>
2020-10-02 16:24:28 -0700

    check etag before downloading for list urls

--- src/helpers.rs
+++ src/helpers.rs
@@ -8,20 +8,47 @@ use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
 };
 
-// Name your user agent after your app?
 static APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
 
-pub async fn download<S: AsRef<str>>(url: S) -> Result<Bytes> {
+async fn get<S: AsRef<str>>(url: S) -> Result<(Bytes, Option<String>)> {
     let client = reqwest::Client::builder()
         .user_agent(APP_USER_AGENT)
         .build()?;
 
-    let bytes = client.get(url.as_ref()).send().await?.bytes().await?;
+    let response = client.get(url.as_ref()).send().await?;
 
-    Ok(bytes)
+    let etag = response
+        .headers()
+        .get(reqwest::header::ETAG)
+        .and_then(|value| value.to_str().ok())
+        .map(|value| value.to_string());
+
+    let bytes = response.bytes().await?;
+
+    Ok((bytes, etag))
 }
 
-pub async fn cache_download<S: AsRef<str>, P: AsRef<Path>>(url: S, cache_path: P) -> Result<Bytes> {
+async fn get_head_etag<S: AsRef<str>>(url: S) -> Result<Option<String>> {
+    let client = reqwest::Client::builder()
+        .user_agent(APP_USER_AGENT)
+        .build()?;
+
+    let response = client.head(url.as_ref()).send().await?;
+
+    let etag = response
+        .headers()
+        .get(reqwest::header::ETAG)
+        .and_then(|value| value.to_str().ok())
+        .map(|value| value.to_string());
+
+    Ok(etag)
+}
+
+pub async fn cache_download<S: AsRef<str>, P: AsRef<Path>>(
+    url: S,
+    cache_path: P,
+    use_etag: bool,
+) -> Result<Bytes> {
     let mut hasher = Sha256::new();
     hasher.update(url.as_ref());
 
@@ -40,11 +67,51 @@ pub async fn cache_download<S: AsRef<str>, P: AsRef<Path>>(url: S, cache_path: P
     }
 
     let file_path = dir_path.join(hex_filename);
-    let bytes = if fs::metadata(&file_path)
+    let etag_file_path = dir_path.join(format!("{}.etag", hex_filename));
+
+    let file_cached = fs::metadata(&file_path)
         .await
         .map(|meta| meta.is_file() && meta.len() != 0)
-        .unwrap_or(false)
-    {
+        .unwrap_or(false);
+
+    let etag_matches = if use_etag {
+        let etag_file_cached = fs::metadata(&etag_file_path)
+            .await
+            .map(|meta| meta.is_file() && meta.len() != 0)
+            .unwrap_or(false);
+
+        // if we have everything cached
+        if file_cached && etag_file_cached {
+            let mut etag_file = OpenOptions::new()
+                .read(true)
+                .open(&etag_file_path)
+                .await
+                .chain_err(|| "open etag_file_path")?;
+
+            let mut s = String::new();
+            etag_file.read_to_string(&mut s).await?;
+
+            let maybe_etag = get_head_etag(&url).await?;
+
+            if let Some(etag) = maybe_etag {
+                // check if old matches current
+                s == etag
+            } else {
+                // didn't have header, can't use cache
+                false
+            }
+        } else {
+            // didn't have either etag file cached or data file cached
+            false
+        }
+    } else {
+        // pretend it matches
+        true
+    };
+
+    let use_cached = file_cached && etag_matches;
+
+    let bytes = if use_cached {
         let mut file = OpenOptions::new().read(true).open(&file_path).await?;
 
         let mut vec = Vec::new();
@@ -52,10 +119,18 @@ pub async fn cache_download<S: AsRef<str>, P: AsRef<Path>>(url: S, cache_path: P
 
         Bytes::from(vec)
     } else {
-        let bytes = download(url).await?;
+        let (bytes, maybe_etag) = get(&url).await?;
 
         let mut file = File::create(&file_path).await?;
         file.write_all(&bytes).await?;
+
+        if use_etag {
+            if let Some(etag) = maybe_etag {
+                let mut file = File::create(&etag_file_path).await?;
+                file.write_all(etag.as_bytes()).await?;
+            } else {
+            }
+        }
 
         bytes
     };
--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,7 @@ mod modifiers;
 mod parser;
 
 pub use self::error::{Error, ErrorKind};
-use self::{
-    error::*,
-    helpers::{cache_download, download},
-};
+use self::{error::*, helpers::cache_download};
 use async_trait::async_trait;
 use bytes::Bytes;
 use rand::prelude::*;
@@ -70,7 +67,7 @@ impl Chatsound {
             self.repo, self.repo_path, self.sound_path
         );
 
-        let bytes = cache_download(&url, cache_path).await?;
+        let bytes = cache_download(&url, cache_path, false).await?;
 
         Ok(LoadedChatsound { bytes })
     }
@@ -203,7 +200,7 @@ impl Chatsounds {
             repo
         );
 
-        let bytes = download(api_url).await?;
+        let bytes = cache_download(api_url, &self.cache_path(), true).await?;
         let mut trees: GitHubApiTrees = serde_json::from_slice(&bytes)?;
 
         for entry in trees.tree.iter_mut() {
@@ -241,7 +238,7 @@ impl Chatsounds {
             repo, repo_path
         );
 
-        let bytes = download(msgpack_url).await?;
+        let bytes = cache_download(msgpack_url, &self.cache_path(), true).await?;
         let mut entries: Vec<Vec<String>> = rmp_serde::decode::from_slice(&bytes)?;
 
         for entry in entries.drain(..) {
