1ea081853bfe47fc8f31065dd33a686bd824daff
SpiralP <SpiralP@users.noreply.github.com>
2022-04-08 03:37:04 +0000

    deploy: 3d28d1bb8438da8a2dea50bb8081f717c4c5f8fc

---
+++ alsa/nix/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"mod":[["errno",""]],"type":[["Error","Nix’s main error type."]]});
--- implementors/core/fmt/trait.Write.js
+++ implementors/core/fmt/trait.Write.js
@@ -1,4 +1,3 @@
 (function() {var implementors = {};
-implementors["arrayvec"] = [{"text":"impl&lt;A&gt; Write for ArrayString&lt;A&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;A: Array&lt;Item = u8&gt; + Copy,&nbsp;</span>","synthetic":false,"types":[]}];
-implementors["bytes"] = [{"text":"impl Write for BytesMut","synthetic":false,"types":[]}];
+implementors["bytes"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Write.html\" title=\"trait core::fmt::Write\">Write</a> for <a class=\"struct\" href=\"bytes/struct.BytesMut.html\" title=\"struct bytes::BytesMut\">BytesMut</a>","synthetic":false,"types":["bytes::bytes_mut::BytesMut"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
