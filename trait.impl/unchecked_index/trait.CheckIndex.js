8a7d566dcebff8b3149079671b41beff1a065779
SpiralP <SpiralP@users.noreply.github.com>
2024-08-28 06:56:39 +0000

    deploy: 4164a627cc4cb808b19776a9d041469bcc4c7ca1

--- trait.impl/unchecked_index/trait.CheckIndex.js
+++ trait.impl/unchecked_index/trait.CheckIndex.js
@@ -1,3 +1,9 @@
-(function() {var implementors = {
-"unchecked_index":[]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {
+    var implementors = Object.fromEntries([["unchecked_index",[]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[22]}
--- type.impl/std/primitive.u64.js
+++ type.impl/std/primitive.u64.js
@@ -1,5 +1,9 @@
-(function() {var type_impls = {
-"openssl_sys":[],
-"serde":[],
-"symphonia_core":[]
-};if (window.register_type_impls) {window.register_type_impls(type_impls);} else {window.pending_type_impls = type_impls;}})()
+(function() {
+    var type_impls = Object.fromEntries([["openssl_sys",[]],["serde",[]],["symphonia_core",[]]]);
+    if (window.register_type_impls) {
+        window.register_type_impls(type_impls);
+    } else {
+        window.pending_type_impls = type_impls;
+    }
+})()
+//{"start":55,"fragment_lengths":[18,13,22]}
