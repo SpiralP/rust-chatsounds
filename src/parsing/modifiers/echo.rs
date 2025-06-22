131dbf753d473c95cb00955700af7848ee3ee227
SpiralP <spiral.palmer@gmail.com>
2025-06-21 18:34:51 -0700

    feat(echo): add arg 3 `amount` to `echo` (defaults to 1)
    How many times to reverb.

--- src/parsing/modifiers/echo.rs
+++ src/parsing/modifiers/echo.rs
@@ -1,7 +1,7 @@
 use std::time::Duration;
 
 use nom::{branch::alt, bytes::complete::tag, IResult, Parser};
-use rodio::Source;
+use rodio::{dynamic_mixer::mixer, Source};
 
 use super::{parse_args, ModifierTrait};
 use crate::BoxSource;
@@ -11,6 +11,7 @@ pub struct EchoModifier {
     /// in seconds
     pub duration: f32,
     pub amplitude_diff: f32,
+    pub amount: u8,
 }
 
 impl Default for EchoModifier {
@@ -18,6 +19,7 @@ impl Default for EchoModifier {
         Self {
             duration: 0.25,
             amplitude_diff: -0.5,
+            amount: 1,
         }
     }
 }
@@ -39,15 +41,30 @@ impl ModifierTrait for EchoModifier {
             modifier.amplitude_diff = -1.0 * amplitude;
         }
 
+        #[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
+        if let Some(amount) = args.get(2).copied().unwrap_or(None) {
+            modifier.amount = amount.max(1.0).floor() as u8;
+        }
+
         Ok((input, modifier))
     }
 
     fn modify(&self, source: BoxSource) -> BoxSource {
-        let duration = Duration::from_secs_f32(self.duration);
-        Box::new(
-            source
-                .buffered()
-                .reverb(duration, 1.0 + self.amplitude_diff),
-        )
+        let mut amplitude = 1.0 + self.amplitude_diff;
+        let mut duration = Duration::from_secs_f32(self.duration);
+
+        let (controller, mixer) = mixer(source.channels(), source.sample_rate());
+
+        let a = source.buffered();
+        controller.add(a.clone());
+
+        for _i in 0..self.amount {
+            let echo = a.clone().amplify(amplitude).delay(duration);
+            controller.add(echo);
+            amplitude = (amplitude + self.amplitude_diff).max(0.0);
+            duration += Duration::from_secs_f32(self.duration);
+        }
+
+        Box::new(mixer)
     }
 }
